24282c2bd46ea5307b86f46d0f2eec216634e5ea
mhaleighw <mhaleighwong@gmail.com>
2024-02-08 13:31:21 -0600

    updated homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
+// import About from './components/About'
+
 
 function App() {
   return (

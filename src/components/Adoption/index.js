ddd3c20c6b5dea1a5e78377aa9a827576b2ab18b
mhaleighw <mhaleighwong@gmail.com>
2024-02-10 18:00:46 -0600

    changes to adoption page

--- src/components/Adoption/index.js
+++ src/components/Adoption/index.js
@@ -9,51 +9,50 @@ const Adoption = () => {
 
     useEffect(() => {
         const idTimeOut = setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 3000)
+            setLetterClass('text-animate-hover');
+        }, 3000);
         return () => clearTimeout(idTimeOut);
-    }, [])
+    }, []);
 
     const renderAdoption = (dogs) => {
         return (
             <div className="images-container">
-                {
-                    dogs.map((dog, idx) => {
-                        return (
-                            <div className="image-box" key={idx}>
-                                <img 
-                                    src={dog.cover}
-                                    className="dog-image"
-                                    alt="dog"
-                                />
-                                <div className="content">
-                                    <p className="title">{dog.title}</p>
-                                    <h4 className="description">{dog.description}</h4>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+                {dogs.map((dog, idx) => (
+                    <div className="image-box" key={idx}>
+                        <div className="dog-info">
+                            <img 
+                                src={dog.cover}
+                                className="dog-image"
+                                alt="dog"
+                            />
+                        <div className="content">
+                            <p><strong>Name:</strong> {dog.title}</p>
+                            <p><strong>Age:</strong> {dog.age}</p>
+                            <p><strong>Description:</strong> {dog.description}</p>
+                            <p><strong>Price:</strong> {dog.price}</p>
+                        </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         );
     }    
 
-return (
-    <>
-        <div className="container adoption-page">
-            <h1 className="page-title">
-                <AnimatedLetters
-                letterClass={letterClass}
-                strArray={"Adopt a Frenchie!".split("")}
-                idx={10}
-            />
-        </h1>
-        <div>{renderAdoption(dogsData.dogs)}</div>
-    </div>
-    <Loader type="pacman" />
-</>
-);
+    return (
+        <>
+            <div className="container adoption-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Adopt a Frenchie!".split("")}
+                        idx={10}
+                    />
+                </h1>
+                <div>{renderAdoption(dogsData.dogs)}</div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    );
 }
 
 export default Adoption;
-

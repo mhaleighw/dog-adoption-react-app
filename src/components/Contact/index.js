af0ff21ba4de85c95c419b4e0c1c7a2dc0536be7
mhaleighw <mhaleighwong@gmail.com>
2024-02-28 14:22:01 -0600

    adjusted navigation bar

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,7 +8,7 @@ const Contact = () => {
         <div className='container contact-page'>
             <div className='text-zone'>
                 <h1>
-                   <AnimatedLetters strArray={"Contact us!".split("")} idx={10} letterClass="text-animate"/> 
+                   <AnimatedLetters strArray={"Contact us!".split("")} idx={.5} letterClass="text-animate"/> 
                 </h1>
 
             </div>
